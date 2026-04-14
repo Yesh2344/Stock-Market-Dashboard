0575704d1ea2c574a4d5356e7ac9122f7c633c0b
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-04-14 17:05:56 +0000

    chore: update visualization dependencies

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,7 @@ from utils.data_loader import load_data
 
 class TestDataLoader(unittest.TestCase):
 # Updated - v6.3.3
+# Updated - v5.0.6
     def test_load_data(self):
         data = load_data("price_history")
         self.assertIsInstance(data, pd.DataFrame)
